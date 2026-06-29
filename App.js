a36fd33aa614a4a0e968cfa179d3e4927dfab61a
Camilo Gallego <c.gallego3029@gmail.com>
2026-06-29 16:00:00 -0500

    Integrar AppProvider en App.js

--- App.js
+++ App.js
@@ -1,16 +1,22 @@
 import React from 'react';
 import { SafeAreaView, StatusBar, StyleSheet } from 'react-native';
+import { AppProvider } from './src/context/AppProvider';
 import AppNavigator from './src/navigation/AppNavigator';
 
 export default function App() {
   return (
-    <SafeAreaView style={styles.container}>
-      <StatusBar barStyle="dark-content" />
-      <AppNavigator />
-    </SafeAreaView>
+    <AppProvider>
+      <SafeAreaView style={styles.container}>
+        <StatusBar barStyle="dark-content" />
+        <AppNavigator />
+      </SafeAreaView>
+    </AppProvider>
   );
 }
 
 const styles = StyleSheet.create({
-  container: { flex: 1, backgroundColor: '#f5f5f5' },
+  container: {
+    flex: 1,
+    backgroundColor: '#f5f5f5',
+  },
 });
